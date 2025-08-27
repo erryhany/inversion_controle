c6ae45c8179469d21301be3e44a76da316d905aa
erryhany <mourad.erryhany@gmail.com>
2025-08-27 13:14:20 +0100

    Initial commit - Inversion de Contrôle avec README professionnel

---
+++ src/main/java/com/mourad/inversion_controle/InversionControleApplication.java
@@ -0,0 +1,13 @@
+package com.mourad.inversion_controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InversionControleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InversionControleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mourad/inversion_controle/InversionControleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mourad.inversion_controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversionControleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
